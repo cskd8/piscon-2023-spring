aa6a69a3e8a096868009ddf92cd568b1673158d4
cskd8 <mh4869203@gmail.com>
2023-02-24 14:23:51 +0000

    revert

--- go/main.go
+++ go/main.go
@@ -839,8 +839,7 @@ func getBookHandler(c echo.Context) error {
 		Book: book,
 	}
 
-	// select only one field
-	err = tx.GetContext(c.Request().Context(), &Lending{}, "SELECT `id` FROM `lending` WHERE `book_id` = ?", id)
+	err = tx.GetContext(c.Request().Context(), &Lending{}, "SELECT * FROM `lending` WHERE `book_id` = ?", id)
 	if err == nil {
 		res.Lending = true
 	} else if errors.Is(err, sql.ErrNoRows) {
