183399f20a6bff1d14be65db3a5f0e2a11921d45
cskd8 <mh4869203@gmail.com>
2023-02-24 13:05:25 +0000

    fix insert

--- go/main.go
+++ go/main.go
@@ -975,10 +975,16 @@ func postLendingsHandler(c echo.Context) error {
 		books = append(books, book)
 	}
 
-	// Insert
-	_, err = tx.ExecContext(c.Request().Context(),
-		"INSERT INTO `lending` (`id`, `book_id`, `member_id`, `due`, `created_at`) VALUES (?, ?, ?, ?, ?)",
-		ids, bookIDs, memberIDs, dues, lendingTimes)
+	// bulk insert using ids, bookIDs, memberIDs, dues, lendingTimes
+	_, err = tx.NamedExecContext(c.Request().Context(),
+		"INSERT INTO `lending` (`id`, `book_id`, `member_id`, `due`, `lending_time`) VALUES (:id, :book_id, :member_id, :due, :lending_time)",
+		map[string]interface{}{
+			"id":           ids,
+			"book_id":      bookIDs,
+			"member_id":    memberIDs,
+			"due":          dues,
+			"lending_time": lendingTimes,
+		})
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
